1425fb306a883115384c1ef0721fbcc08062d4b8
Vijay Patel <vijaygurjarppi@gmail.com>
2025-05-16 06:52:32 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -76,3 +76,25 @@ int main()
     obj.cube();
     return 0;
 }
+
+
+3 array +loop
+    #include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Reversed array:\n";
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+
+    return 0;
+}

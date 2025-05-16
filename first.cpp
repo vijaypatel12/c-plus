e794f5649c25ac93601364ddf02e0a45fa4198b8
Vijay Patel <vijaygurjarppi@gmail.com>
2025-05-16 05:47:10 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 using namespace std;
 
-// Class Definition
 class Addition {
 private:
-    int a, b;  // Data members
+    int a, b;
 
 public:
-    // Function to input numbers
     void getData() {
         cout << "Enter two numbers: ";
         cin >> a >> b;
     }
 
-    // Function to perform addition
     void add() {
-        int sum = a + b;
-        cout << "Sum = " << sum << endl;
+        cout << "Sum = " << a + b << endl;
     }
 };
 
-// Main Function
 int main() {
-    Addition obj;     // Creating object of class
-    obj.getData();    // Taking input using object
-    obj.add();        // Performing addition using object
+    Addition obj;
+    obj.getData();
+    obj.add();
     return 0;
 }
+
 
 
 2

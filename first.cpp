94d30df36fe9d1468c3a6bd2b5a9787ef0139532
Vijay Patel <vijaygurjarppi@gmail.com>
2025-05-16 06:58:43 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -98,3 +98,27 @@ int main() {
 
     return 0;
 }
+ 4 array +loop even odd
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, even = 0, odd = 0;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    cout << "Even numbers: " << even << endl;
+    cout << "Odd numbers: " << odd << endl;
+    return 0;
+}

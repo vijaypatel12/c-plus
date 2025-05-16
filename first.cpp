248e0b3c36bf16f380fa5c9f4db5fe9229827699
Vijay Patel <vijaygurjarppi@gmail.com>
2025-05-16 05:32:50 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -27,3 +27,56 @@ int main() {
     obj.add();        // Performing addition using object
     return 0;
 }
+
+
+2
+    class object area rectangle ,cube,circle
+
+    #include <iostream>
+using namespace std;
+
+class area
+{
+   private:
+           int a,l,w,r;   //data members
+                                          //data encapsulation
+   public:
+         
+          int circle()                              //member function
+          {
+              cout<<"enter the radius:"<<endl;
+              cin>>r;
+             int circle1;
+             circle1 = 3.14 * r;
+            cout<<"\nthe area of circle  is = "<<circle1;
+             return 0;
+          }   
+          int rectangle()
+          {
+              cout<<"\nenter the length and width:\n"<<endl;
+              cin>>l>>w;
+             int rectangle1;
+             rectangle1 = l*w;
+            cout<<"\nthe area of rectangle  is = "<<rectangle1;
+             return 0;
+          }
+           int cube()
+          {
+              cout<<"\nenter the area:\n"<<endl;
+              cin>>a;
+             int cube1;
+             cube1 = 6*a*a*a;
+            cout<<"\nthe area of cube  is = "<<cube1;
+             return 0;
+          }
+};
+
+int main()
+{
+    area obj;
+                //creation of object
+    obj.circle();
+    obj.rectangle();
+    obj.cube();
+    return 0;
+}

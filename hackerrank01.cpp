97b352484e0d3c8ff4e730333156bc8fc13ece6f
Vijay Patel <vijaygurjarppi@gmail.com>
2025-05-20 14:57:19 +0530

    Create hackerrank01.cpp

---
+++ hackerrank01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    string words[]={"one","two","three","four","five","six","seven","eight","nine"};
+    for(int i=a;i<=b;i++){
+        if(i>=1 && i<=9){
+            cout<<words[i-1]<<endl;
+        }
+        else if(i>9 && i%2==0){
+            cout<<"even"<<endl;
+        }
+        else{
+            cout<<"odd"<<endl;
+        }
+    }
+    return 0;
+}
